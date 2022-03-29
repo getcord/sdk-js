099e1b52bda4806b9adb4acd2f38d4a1c06d4233
Adam Vartanian <flooey@cord.com>
2022-03-29 12:09:04 +0100

    Add start of server library: makeClientAuthToken (#661)
    Adds a new @cord-sdk/server package for helping with server-side
    operations.  Only one function so far: makeClientAuthToken.  It
    doesn't do much (just signs the token properly), but it should
    eliminate some common mistakes.
    
    I started using it in all the places we use the SDK, it makes the code
    a little simpler.
    
    Test Plan:
    
    * npm run build
    * npm run test
    * Loaded the local admin tool, it logged me in properly
    
    monorepo-commit: 9c5b205286275be656083e9f8d60ab0c35386d2d

---
+++ packages/server/index.ts
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+export type PlatformUserVariables = {
+  email: string;
+  name?: string;
+  profile_picture_url?: string;
+  status?: 'active' | 'deleted';
+  first_name?: string;
+  last_name?: string;
+};
+
+export type PlatformOrganizationVariables = {
+  name: string;
+  status?: 'active' | 'deleted';
+  members?: Array<string>;
+};
+
+export type ClientAuthTokenData = {
+  app_id: string;
+  user_id: string;
+  organization_id: string;
+  user_details?: PlatformUserVariables;
+  organization_details?: PlatformOrganizationVariables;
+};
+
+export function makeClientAuthToken(
+  app_id: string,
+  app_secret: string,
+  payload: Omit<ClientAuthTokenData, 'app_id'>,
+): string {
+  return jwt.sign({ ...payload, app_id }, app_secret, {
+    algorithm: 'HS512',
+    expiresIn: '1 min',
+  });
+}
