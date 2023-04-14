c3a1188755724eb47c58a957c821e705aba4a038
Nick Filippakis <102963329+nickfil22@users.noreply.github.com>
2023-04-14 17:01:23 +0100

    Refactoring the ThreadFacepile component
    While creating the component, in #4281,  @jwatzman suggested that I filter out null or undefined
    values before rendering the component. I missed his comment and landed that PR, hence I am creating this one
    to apply his suggestion.
    
    Test Plan:
    Checked the ThreadFacepile component in the testbed, worked fine
    
    
    Reviewers: jwatzman, flooey
    
    Reviewed By: jwatzman
    
    Pull Request: https://github.com/getcord/monorepo/pull/4315
    
    monorepo-commit: caf1eb14735e192ce4b30d3da1a2546058070b4e

--- packages/react/components/ThreadFacepile.tsx
+++ packages/react/components/ThreadFacepile.tsx
@@ -5,6 +5,12 @@ export function ThreadFacepile({ threadId }: { threadId: string }) {
   // TODO (nickfil22): Remove nullables from our ThreadParticipant GQL object.
   // If we have thread participants, it means we also have and id and timestamp.
   // We shouldn't have to null check here.
-  const userIDs = threadSummary?.participants.map((u) => u.userID ?? '') ?? [];
+  const userIDs = (threadSummary?.participants ?? [])
+    .map((u) => u.userID)
+    .filter(isDefined);
   return <experimental.Facepile users={userIDs} />;
+}
+
+function isDefined<T>(value: T | null | undefined): value is T {
+  return value !== null && value !== undefined;
 }
