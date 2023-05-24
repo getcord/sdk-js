c9be32ffe579f9df93412feb269e6aaf89b24f7c
Adam Vartanian <flooey@cord.com>
2023-05-24 16:35:50 +0100

    Add User API docs (#4730)
    Adds docs for the new user API.
    
    Test Plan:
    
    monorepo-commit: 74193dcf469a053463318e38ab415b00cdbed6d6

--- packages/react/hooks/user.ts
+++ packages/react/hooks/user.ts
@@ -18,7 +18,74 @@ function sameIDs(left: string | string[], right: string | string[]): boolean {
   return left === right;
 }
 
+/**
+ * This method allows you to observe data about a user, including live updates.
+ * @example Overview
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = user.useUserData(userID);
+ * ```
+ * @example Usage
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = user.useUserData('user-123');
+ * return (
+ *   <div>
+ *     {!data && "Loading..."}
+ *     {data && (
+ *       <p>User name: {data.name}</p>
+ *       <p>User short name: {data.shortName}</p>
+ *       <p>User profile picture: <img src={data.profilePictureURL} /></p>
+ *     )}
+ *   </div>
+ * );
+ * ```
+ * @param userID - The user to fetch data for.
+ * @returns The hook will initially return `undefined` while the data loads from
+ * our API. Once it has loaded, your component will re-render and the hook will
+ * return an object containing the fields described under "Available Data"
+ * above. The component will automatically re-render if any of the data changes,
+ * i.e., this data is always "live".
+ */
 export function useUserData(userID: string): UserData | null | undefined;
+
+/**
+ * This method allows you to observe data about multiple users, including live
+ * updates.
+ * @example Overview
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = user.useUserData(userIDs);
+ * ```
+ * @example Usage
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = user.useUserData(['user-123', 'user-456']);
+ * return (
+ *   <div>
+ *     {!data && "Loading..."}
+ *     {data && (
+ *       {Object.entries(data).map(([id, userData]) => (
+ *         <div key={id}>
+ *           <p>User ID: {id}</p>
+ *           <p>User name: {userData.name}</p>
+ *           <p>User profile picture: <img src={userData.profilePictureURL} /></p>
+ *         </div>
+ *       ))}
+ *     )}
+ *   </div>
+ * );
+ * ```
+ * @param userIDs - The list of user IDs to fetch data for.
+ * @returns The hook will initially return an empty object while the data loads
+ * from our API. Once some data has loaded, your component will re-render and
+ * the hook will return an object with a property for each requested user ID. If
+ * the property is missing, the data for that user has not yet been loaded; if
+ * there's no user with that ID, it will be `null`; and otherwise it will be an
+ * object which will contain the fields described under "Available Data" above.
+ * The component will automatically re-render if any of the data changes or as
+ * more data is loaded, i.e., this data is always "live".
+ */
 export function useUserData(userIDs: string[]): Record<string, UserData | null>;
 export function useUserData(
   userIDorIDs: string | string[],
@@ -62,6 +129,36 @@ export function useUserData(
   return data;
 }
 
+/**
+ * This method allows you to observe data about the logged-in user, including
+ * live updates.
+ * @example Overview
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = thread.useViewerData();
+ * ```
+ * @example Usage
+ * ```javascript
+ * import { user } from '@cord-sdk/react';
+ * const data = thread.useViewerData();
+ * return (
+ *   <div>
+ *     {!data && "Loading..."}
+ *     {data && (
+ *       <p>User name: {data.name}</p>
+ *       <p>User short name: {data.shortName}</p>
+ *       <p>User profile picture: <img src={data.profilePictureURL} /></p>
+ *       <p>Organization ID: {data.organizationID}</p>
+ *     )}
+ *   </div>
+ * );
+ * ```
+ * @returns The hook will initially return `undefined` while the data loads from
+ * our API. Once it has loaded, your component will re-render and the hook will
+ * return an object containing the fields described under "Available Data"
+ * above. The component will automatically re-render if any of the data changes,
+ * i.e., this data is always "live".
+ */
 export function useViewerData(): ViewerUserData | undefined {
   const { sdk } = useCordContext('user.useViewerData');
   const userSDK = sdk?.user;
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -242,16 +242,102 @@ export type MultipleUserUpdateCallback = (
 export type ViewerUserUpdateCallback = (user: ViewerUserData) => unknown;
 
 export interface ICordUserSDK {
+  /**
+   * This method allows you to observe data about a user, including live
+   * updates.
+   * @example Overview
+   * ```javascript
+   * const ref = window.CordSDK.user.observeUserData(userID, callback);
+   * window.CordSDK.user.unobserveUserData(ref);
+   * ```
+   * @example Usage
+   * ```javascript
+   * const ref = window.CordSDK.user.observeUserData(
+   *   'user-123',
+   *   (data) => {
+   *     // Received an update!
+   *     console.log("User name", data.name);
+   *     console.log("User short name", data.shortName);
+   *     console.log("User profile picture URL", data.profilePictureURL);
+   *   }
+   * );
+   * ```
+   * @param userID - The user to fetch data for.
+   * @param callback - This callback will be called once with the current data,
+   * and then again every time the data changes.  The argument passed to the
+   * callback is an object which will contain the fields described under
+   * "Available Data" above.  If there's no user with that ID, the callback will
+   * be called with `null`.
+   * @returns A reference number which can be passed to `unobserveUserData` to
+   * stop observing user data.
+   */
   observeUserData(
     userID: string,
     callback: SingleUserUpdateCallback,
   ): ListenerRef;
+  /**
+   * This method allows you to observe data about multiple users, including live
+   * updates.
+   * @example Overview
+   * ```javascript
+   * const ref = window.CordSDK.user.observeUserData(userIDs, callback);
+   * window.CordSDK.user.unobserveUserData(ref);
+   * ```
+   * @example Usage
+   * ```javascript
+   * const ref = window.CordSDK.user.observeUserData(
+   *   ['user-123', 'user-456'],
+   *   (data) => {
+   *     // Received an update!
+   *     console.log("User-123 name", data['user-123']?.name);
+   *     console.log("User-456 name", data['user-456']?.name);
+   *   }
+   * );
+   * ```
+   * @param userIDs - The list of user IDs to fetch data for.
+   * @param callback - This callback will be called once with the current data,
+   * and then again every time the data changes.  The argument passed to the
+   * callback is an object with a property for each requested user ID.  If the
+   * property is missing, the data for that user has not yet been loaded; if
+   * there's no user with that ID, it will be `null`; and otherwise it will be
+   * an object which will contain the fields described under "Available Data"
+   * above.
+   * @returns A reference number which can be passed to `unobserveUserData` to
+   * stop observing user data.
+   */
   observeUserData(
     userIDs: Array<string>,
     callback: MultipleUserUpdateCallback,
   ): ListenerRef;
   unobserveUserData(ref: ListenerRef): boolean;
 
+  /**
+   * This method allows you to observe data about the logged-in user, including
+   * live updates.
+   * @example Overview
+   * ```javascript
+   * const ref = window.CordSDK.user.observeViewerData(callback);
+   * window.CordSDK.user.unobserveViewerData(ref);
+   * ```
+   * @example Usage
+   * ```javascript
+   * const ref = window.CordSDK.user.observeViewerData(
+   *   (data) => {
+   *     // Received an update!
+   *     console.log("User name", data.name);
+   *     console.log("User short name", data.shortName);
+   *     console.log("User profile picture URL", data.profilePictureURL);
+   *     console.log("Organization ID", data.organizationID);
+   *   }
+   * );
+   * ```
+   * @param callback - This callback will be called once with the current data,
+   * and then again every time the data changes.  The argument passed to the
+   * callback is an object which will contain the fields described under
+   * "Available Data" above.
+   * @returns A reference number which can be passed to `unobserveViewerData` to
+   * stop observing the data.
+   */
   observeViewerData(callback: ViewerUserUpdateCallback): ListenerRef;
   unobserveViewerData(ref: ListenerRef): boolean;
 }
@@ -811,17 +897,42 @@ export function locationJson(c: Partial<Location>): string {
   );
 }
 
-export type UserData = {
+/**
+ * The data associated with a Cord user.
+ */
+export interface UserData {
+  /**
+   * The user's ID.  This is unique within an application.
+   */
   id: string;
+  /**
+   * The user's name.
+   */
   name: string | null;
+  /**
+   * The user's short name.  In most cases, Cord components will prefer using
+   * this name over `name` when set.
+   */
   shortName: string | null;
+  /**
+   * A URL to the user's profile picture.
+   */
   profilePictureURL: string | null;
+  /**
+   * Any metadata that has been set for the user.
+   */
   metadata: EntityMetadata;
-};
+}
 
-export type ViewerUserData = UserData & {
+/**
+ * The data associated with the Cord user that's currently logged in.
+ */
+export interface ViewerUserData extends UserData {
+  /**
+   * The identifier for the organization that the current user is using.
+   */
   organizationID: string;
-};
+}
 
 export interface HTMLCordElement extends HTMLElement {
   initialised?: true;
