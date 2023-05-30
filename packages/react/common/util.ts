6101da104d05f5dc766831b2d4ce19479ae9fcca
Nick Filippakis <102963329+nickfil22@users.noreply.github.com>
2023-05-30 12:58:45 +0100

    Moving the pluralize util functions to the open-source package
    Will be needing this for https://github.com/getcord/monorepo/pull/4696
    
    I moved the functions, then manually fixed all errors since I did not move the whole file.
    
    Test Plan:
    Tested in https://github.com/getcord/monorepo/pull/4696.
    Also clicked around in the testbed, created a thread/annotation and in the local docs.
    
    
    Reviewers: jwatzman, flooey
    
    Reviewed By: jwatzman
    
    Pull Request: https://github.com/getcord/monorepo/pull/4697
    
    monorepo-commit: a7b01b245c959cabef5130acdc0efd540dc15d43

--- packages/react/common/util.ts
+++ packages/react/common/util.ts
@@ -5,3 +5,31 @@
 export function cordifyClassname(className: string) {
   return `cord-${className}`;
 }
+
+/**
+ * Returns a work in singular or plural form depending on given number. This includes the count, e.g.:
+ * - `pluralize(0, 'hour')` -> '0 hours'
+ * - `pluralize(1, 'hour')` -> '1 hour'
+ * - `pluralize(2, 'hour')` -> '2 hours'
+ * - `pluralize(0, 'box', 'boxes')` -> '0 boxes'
+ * - `pluralize(1, 'box', 'boxes')` -> '1 box'
+ */
+export function pluralize(n: number, what: string, plural?: string) {
+  return `${n} ${pluralizeWord(n, what, plural)}`;
+}
+
+/**
+ * Returns a work in singular or plural form depending on given number. This does not include the count, e.g.:
+ * - `pluralizeWord(0, 'hour')` -> 'hours'
+ * - `pluralizeWord(1, 'hour')` -> 'hour'
+ * - `pluralizeWord(2, 'hour')` -> 'hours'
+ * - `pluralizeWord(0, 'box', 'boxes')` -> 'boxes'
+ * - `pluralizeWord(1, 'box', 'boxes')` -> 'box'
+ */
+export function pluralizeWord(
+  n: number,
+  what: string,
+  plural: string = what + 's',
+) {
+  return n === 1 ? what : plural;
+}
