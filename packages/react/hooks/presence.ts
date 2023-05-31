a8ff08dd796102386ca87d40457b5d266a1dd1ca
Adam Vartanian <flooey@cord.com>
2023-05-31 15:04:55 +0100

    Realign usePresenceData to JS API (#4829)
    We now have a public user API, so we don't need to mix in user data in
    the presence hook.  This also makes it match the JS API, which is the
    standard pattern we use.
    
    Test Plan:
    
    Opened testbed, the presence listing functions work the same as before
    
    monorepo-commit: a855a0771710de8ca8b7524935b55933a098b591

--- packages/react/hooks/presence.ts
+++ packages/react/hooks/presence.ts
@@ -1,233 +1,46 @@
 import { locationJson } from '@cord-sdk/types';
 import type {
   Location,
-  ListenerRef,
-  UserPresenceInformation,
-  PartialUserLocationData,
-  UserData,
+  ObservePresenceOptions,
+  UserLocationData,
 } from '@cord-sdk/types';
-import { useEffect, useMemo, useRef, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { useCordContext } from '../contexts/CordContext';
-
-type Options = {
-  excludeViewer?: boolean;
-  onlyPresentUsers?: boolean;
-  partialMatch?: boolean;
-  includeUserDetails?: boolean;
-};
 
 export function useLocationData(
   location: Location,
-  options: Options = {},
-): Array<UserData & UserPresenceInformation> {
-  const {
-    excludeViewer = false,
-    includeUserDetails = false,
-    onlyPresentUsers = false,
-    partialMatch = false,
-  } = options;
-
+  options: ObservePresenceOptions = {},
+): Array<UserLocationData> | undefined {
   const { sdk } = useCordContext('presence.useLocationData');
   const presenceSDK = sdk?.presence;
-  const userSDK = sdk?.user;
 
   const locationString = locationJson(location);
+  const optionsMemo = useMemo(
+    () => ({
+      partial_match: options?.partial_match,
+      exclude_durable: options?.exclude_durable,
+    }),
+    [options?.partial_match, options?.exclude_durable],
+  );
 
-  const [userLocations, setUserLocations] = useState<
-    Record<string, PartialUserLocationData>
-  >({});
-  const [userDetails, setUserDetails] = useState<Record<string, UserData>>({});
-  const userDetailsRef = useRef<Record<string, UserData>>({});
-  userDetailsRef.current = userDetails;
-
-  const [viewerID, setViewerID] = useState<string>();
+  const [presenceData, setPresenceData] = useState<Array<UserLocationData>>();
 
   useEffect(() => {
     if (!presenceSDK) {
       return;
     }
-
-    // eslint-disable-next-line @typescript-eslint/no-shadow -- Disabling for pre-existing problems. Please do not copy this comment, and consider fixing this one!
-    const location = JSON.parse(locationString);
-    let current = true;
-
-    const listenerRef = presenceSDK.addListener(
-      (userLocationUpdate) => {
-        if (!current) {
-          return;
-        }
-
-        // eslint-disable-next-line @typescript-eslint/no-shadow -- Disabling for pre-existing problems. Please do not copy this comment, and consider fixing this one!
-        setUserLocations((userLocations) => ({
-          ...userLocations,
-          [userLocationUpdate.id]: {
-            ...userLocations[userLocationUpdate.id],
-            ...userLocationUpdate,
-          },
-        }));
+    const ref = presenceSDK.observeLocationData(
+      JSON.parse(locationString),
+      (data) => {
+        setPresenceData(data);
       },
-      location,
-      { partial_match: partialMatch },
+      optionsMemo,
     );
 
-    presenceSDK
-      .getPresent(location, {
-        partial_match: partialMatch,
-        exclude_durable: onlyPresentUsers,
-      })
-      .then((userLocationDataList) => {
-        if (current) {
-          setUserLocations(
-            Object.fromEntries(
-              userLocationDataList.map((data) => [data.id, data]),
-            ),
-          );
-        }
-      });
-
     return () => {
-      current = false;
-      presenceSDK.removeListener(listenerRef);
+      presenceSDK.unobserveLocationData(ref);
     };
-  }, [presenceSDK, locationString, partialMatch, onlyPresentUsers]);
+  }, [presenceSDK, locationString, optionsMemo]);
 
-  const userDetailsListenersRef = useRef(new Map<string, ListenerRef>());
-  const mountedRef = useRef(true);
-
-  const currentUserIDs = useMemo(
-    () => new Set(Object.keys(userLocations)),
-    [userLocations],
-  );
-
-  useEffect(() => {
-    if (!userSDK || !includeUserDetails) {
-      return;
-    }
-
-    mountedRef.current = true;
-
-    // make sure all current user IDs are being observed
-    for (const id of currentUserIDs) {
-      if (!userDetailsListenersRef.current.has(id)) {
-        const listenerRef = userSDK.observeUserData(id, (user) => {
-          if (mountedRef.current && user) {
-            // eslint-disable-next-line @typescript-eslint/no-shadow -- Disabling for pre-existing problems. Please do not copy this comment, and consider fixing this one!
-            setUserDetails((userDetails) => ({
-              ...userDetails,
-              [user.id]: user,
-            }));
-          }
-        });
-
-        userDetailsListenersRef.current.set(id, listenerRef);
-      }
-    }
-
-    for (const [id, ref] of userDetailsListenersRef.current.entries()) {
-      if (!currentUserIDs.has(id)) {
-        userSDK.unobserveUserData(ref);
-        userDetailsListenersRef.current.delete(id);
-      }
-    }
-  }, [userSDK, currentUserIDs, includeUserDetails]);
-
-  useEffect(() => {
-    if (!userSDK) {
-      return;
-    }
-
-    mountedRef.current = true;
-
-    // fetch viewer details on mount
-    const viewerRef = userSDK.observeViewerData((viewer) => {
-      if (mountedRef.current) {
-        setViewerID(viewer.id);
-      }
-    });
-
-    // remove user listeners on unmount
-    const listeners = userDetailsListenersRef.current;
-    return () => {
-      mountedRef.current = false;
-      for (const ref of listeners.values()) {
-        userSDK.unobserveUserData(ref);
-      }
-      userSDK.unobserveViewerData(viewerRef);
-    };
-  }, [userSDK]);
-
-  return useMemo<Array<UserData & UserPresenceInformation>>(() => {
-    if (excludeViewer && viewerID === undefined) {
-      // if we're supposed to exclude the viewer, don't return anything
-      // until we know who that is
-      return [];
-    }
-
-    const now = new Date();
-
-    return Object.entries(userLocations)
-      .filter(([id, locationData]) => {
-        if (excludeViewer && viewerID === id) {
-          // exclude the viewer if requested
-          return false;
-        }
-
-        if (includeUserDetails && !userDetails[id]) {
-          // exclude users whose details we don't have yet
-          return false;
-        }
-
-        const present = Boolean(locationData.ephemeral?.locations?.length);
-        if (onlyPresentUsers && !present) {
-          // if the user isn't present in any location for this matcher
-          return false;
-        }
-
-        return true;
-      })
-      .map(([id, locationData]) => ({
-        ...(includeUserDetails
-          ? userDetails[id]
-          : {
-              name: '',
-              shortName: null,
-              profilePictureURL: null,
-              metadata: {},
-            }),
-        id,
-        present: Boolean(locationData.ephemeral?.locations?.length),
-        lastPresent: locationData.durable?.timestamp ?? now,
-        presentLocations: locationData.ephemeral?.locations ?? [],
-      }))
-      .sort((u1, u2) => {
-        if (u1.present && !u2.present) {
-          // present users go before not present ones
-          return -1;
-        } else if (!u1.present && u2.present) {
-          // present users go before not present ones
-          return 1;
-        } else if (!u1.present && !u2.present) {
-          // both users are absent, order by lastPresent
-          return u2.lastPresent.getTime() - u1.lastPresent.getTime();
-        } else {
-          // both users are present, place the viewer first if one of the users is
-          if (u1.id === viewerID) {
-            return -1;
-          } else if (u2.id === viewerID) {
-            return 1;
-          } else {
-            // neither is the viewer, order by id so that the list of present users
-            // is stable (people don't jump around if they update their presence often)
-            return u1.id < u2.id ? -1 : 1;
-          }
-        }
-      });
-  }, [
-    userLocations,
-    userDetails,
-    viewerID,
-    excludeViewer,
-    includeUserDetails,
-    onlyPresentUsers,
-  ]);
+  return presenceData;
 }
