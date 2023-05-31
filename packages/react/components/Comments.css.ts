8767fdfbc1932463c9dde431ad2b45f03b175526
Nick Filippakis <102963329+nickfil22@users.noreply.github.com>
2023-05-31 15:17:13 +0100

    Comments 1: Thread list with composer
    Moving the existing `ThreadedComments` component to our opensource package and renaming it to `Comments`.
    
    This PR builds a thread list, with a composer at the bottom. New threads started from the composer appear immediately in the thread list and are sorted from newest to oldest message (top to bottom). Right now we are only rendering the first message of each thread. In subsequent PRs, I will adding more functionality.
    
    Test Plan:
    Video attached
    
    
    https://github.com/getcord/monorepo/assets/102963329/f998cbc0-49c8-477b-8baa-f550d7a4d0ce
    
    
    
    
    
    Reviewers: AlbertQM, lazybean, jwatzman, flooey
    
    Reviewed By: jwatzman
    
    Pull Request: https://github.com/getcord/monorepo/pull/4696
    
    monorepo-commit: bb584a25cce6c8f24f1714f3ddfa47cbe0497785

--- packages/react/components/Comments.css.ts
+++ packages/react/components/Comments.css.ts
@@ -1,10 +1,61 @@
 import { globalStyle } from '@vanilla-extract/css';
 import { cordifyClassname } from '../common/util';
+import { cssVar } from '../common/ui/cssVariables';
 
-export const dummy = cordifyClassname('dummy');
-globalStyle(`.${dummy}`, {
+export const comments = cordifyClassname('comments');
+globalStyle(`.${comments}`, {
+  width: '320px',
+  border: `1px solid ${cssVar('color-base-x-strong')}`,
+  padding: cssVar('space-2xs'),
+  borderRadius: cssVar('space-3xs'),
+});
+
+export const threadList = cordifyClassname('thread-list');
+globalStyle(`.${threadList}`, {
+  overflow: 'scroll',
+  display: 'flex',
+  flexDirection: 'column',
+  gap: cssVar('space-xs'),
+  height: 'auto',
+});
+
+export const commentsThread = cordifyClassname('comments-thread');
+
+export const firstMessage = cordifyClassname('first-message');
+
+export const hr = cordifyClassname('hr');
+export const threadActionButton = cordifyClassname('thread-action-button');
+globalStyle(`.${threadActionButton}`, {
+  padding: `${cssVar('space-2xs')} calc(${cssVar('space-l')} + ${cssVar(
+    'space-2xs',
+  )})`,
+  margin: `0 ${cssVar('space-3xs')} 0 ${cssVar('space-2xs')}`,
+  borderRadius: cssVar('space-3xs'),
+  border: 'none',
+  background: 'none',
+  textAlign: 'left',
+  cursor: 'pointer',
+  display: 'flex',
+  alignItems: 'center',
+  gap: cssVar('space-2xs'),
+});
+globalStyle(`.${threadActionButton}:hover`, {
+  background: cssVar('color-base-strong'),
+});
+globalStyle(`.${hr}`, {
+  padding: cssVar('space-2xs'),
+});
+globalStyle(`.${hr}::before`, {
   display: 'block',
-  height: '100px',
-  width: '100px',
-  backgroundColor: 'red',
+  content: '',
+  // We need to hardcode the width of the horizontal line to make
+  // sure that the "Show more" text correctly aligns
+  width: '10px',
+  borderTop: `1px solid ${cssVar('color-base-x-strong')}`,
+});
+globalStyle(`.${hr}::after`, {
+  display: 'block',
+  content: '',
+  flexGrow: 1,
+  borderTop: `1px solid ${cssVar('color-base-x-strong')}`,
 });
--- packages/react/components/Comments.tsx
+++ packages/react/components/Comments.tsx
@@ -1,5 +1,59 @@
+import cx from 'classnames';
+import type { Location } from '@cord-sdk/types';
+import * as fonts from '../common/ui/atomicClasses/fonts.css';
 import * as classes from './Comments.css';
+import { Composer } from './Composer';
+import { Message } from './Message';
+import {
+  useLocationData,
+  useThreadSummary,
+} from '@cord-sdk/react/hooks/thread';
 
-export function Comments() {
-  return <div className={classes.dummy}>Hello</div>;
+export function Comments({ location }: { location: Location }) {
+  const { threads, hasMore, fetchMore } = useLocationData(location, {
+    sortBy: 'first_message_timestamp',
+    sortDirection: 'descending',
+    includeResolved: false,
+  });
+
+  return (
+    <div className={classes.comments}>
+      <div className={classes.threadList}>
+        {threads.map((thread) => (
+          <CommentsThread key={thread.id} threadId={thread.id} />
+        ))}
+        {hasMore && (
+          <button
+            className={cx(
+              classes.threadActionButton,
+              classes.hr,
+              fonts.fontSmall,
+            )}
+            onClick={() => fetchMore(5)}
+          >
+            {'Fetch more'}
+          </button>
+        )}
+      </div>
+      <Composer location={location} />
+    </div>
+  );
+}
+
+function CommentsThread({ threadId }: { threadId: string }) {
+  const threadSummary = useThreadSummary(threadId);
+
+  if (!threadSummary || !threadSummary.firstMessage?.id) {
+    return null;
+  }
+
+  return (
+    <div className={classes.commentsThread}>
+      <Message
+        className={classes.firstMessage}
+        messageId={threadSummary.firstMessage?.id}
+        threadId={threadId}
+      />
+    </div>
+  );
 }
