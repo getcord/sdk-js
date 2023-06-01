c2a72573b82f2aacf773b0b697f4ca3d96ce73cb
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-06-01 10:37:08 +0200

    Move modifiers.ts to opensource
    I'll need modifiers as I'm moving `Avatar.css` to `opensource`.
    All other dependencies are already there thanks to Nick!
    
    - Moved `modifiers.ts`
    - Grepped `ui3/modifiers` and updated it to the new path.
    
    
    Reviewers: flooey, lazybean
    
    Reviewed By: lazybean
    
    Pull Request: https://github.com/getcord/monorepo/pull/4817
    
    monorepo-commit: 14c99e5c133771dd68d6701de65d59cb640183af

---
+++ packages/react/common/ui/modifiers.ts
@@ -0,0 +1,26 @@
+import { cordifyClassname } from '@cord-sdk/react/common/util';
+
+export const MODIFIERS = {
+  loading: cordifyClassname('loading'),
+  notPresent: cordifyClassname('not-present'),
+  present: cordifyClassname('present'),
+  noReactions: cordifyClassname('no-reactions'),
+  deleted: cordifyClassname('deleted'),
+  unseen: cordifyClassname('unseen'),
+  editing: cordifyClassname('editing'),
+};
+
+/**
+ * Applies one or more `modifier` class to the `selector`.
+ * The CSS specificity is the one of `selector`, as we remove
+ * the modifier specificity using `:where()`.
+ */
+export function getModifiedSelector(
+  modifiers: keyof typeof MODIFIERS | Array<keyof typeof MODIFIERS>,
+  selector: string,
+) {
+  const modifiersToApply = Array.isArray(modifiers)
+    ? modifiers.map((modifier) => MODIFIERS[modifier]).join('.')
+    : `.${MODIFIERS[modifiers]}`;
+  return `:where(${modifiersToApply})${selector}`;
+}
