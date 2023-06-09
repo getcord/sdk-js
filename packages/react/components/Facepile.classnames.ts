72c87f1e076c6e89d04191ed1982b22ddd910708
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-06-09 12:39:50 +0100

    UI3 Facepile
    First PR of N.
    - Added switch config for new Facepile.
    - Re-implemented horizontal facepile and "+ <x users>" UI
    - Not a breaking change for anyone as this is a new component
    
    What's missing, and coming in other PRs:
    - Vertical facepile
    - Special "+" placeholder in collapsed thread
    - Probably more nits
    
    Test Plan:
    Go to test bed, switch to new Facepile, check it looks pretty much the same!
    
    
    Reviewers: flooey, lazybean
    
    Reviewed By: lazybean
    
    Pull Request: https://github.com/getcord/monorepo/pull/4941
    
    monorepo-commit: 8167c897897e864eb37305c4fcb94179a41407d2

---
+++ packages/react/components/Facepile.classnames.ts
@@ -0,0 +1,7 @@
+import { cordifyClassname } from '../common/util';
+
+export const facepileContainer = cordifyClassname('facepile');
+export const otherUsers = cordifyClassname('other-users');
+export const otherUsersPlaceholder = cordifyClassname(
+  'other-users-placeholder',
+);
