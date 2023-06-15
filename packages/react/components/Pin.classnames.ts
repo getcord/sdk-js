98b535f3c93c059b17a2d93680a37e201357f3a1
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-06-15 12:01:17 +0100

    Docs: Add classnames to cord-pin
    Let's document the classnames available to `cord-pin`.
    
    Test Plan:
    Load docs locally.
    
    
    Reviewers: flooey, lazybean
    
    Reviewed By: lazybean
    
    Pull Request: https://github.com/getcord/monorepo/pull/5085
    
    monorepo-commit: 4acf3491096dd5caf0a443244891f3a80a223c27

---
+++ packages/react/components/Pin.classnames.ts
@@ -0,0 +1,6 @@
+import { cordifyClassname } from '../common/util';
+
+export const pinContainer = cordifyClassname('pin-container');
+export const pinClassnamesDocs = {
+  [pinContainer]: 'Applied to the container div. This class is always present.',
+};
