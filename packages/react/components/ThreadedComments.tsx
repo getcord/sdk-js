4f117d9a200ce23cf5446428fba097b0453dc53e
Josh Watzman <jwatzman@cord.com>
2023-06-15 14:05:35 +0100

    ThreadedComments: add data-cord-thread-id
    This is super useful for debugging and inspecting. Our internal
    components do this, let's have the external one do it too.
    
    
    Test Plan: Inspect the component in the DOM, see the attribute.
    
    Reviewers: flooey, dmmiller
    
    Reviewed By: dmmiller
    
    Pull Request: https://github.com/getcord/monorepo/pull/5099
    
    monorepo-commit: 5fc710a8d75bfc2fad4cfb441039ce8db4b1d160

--- packages/react/components/ThreadedComments.tsx
+++ packages/react/components/ThreadedComments.tsx
@@ -79,7 +79,7 @@ function CommentsThread({ threadId }: { threadId: string }) {
   }
 
   return (
-    <div className={classes.thread}>
+    <div className={classes.thread} data-cord-thread-id={threadId}>
       <Message messageId={threadSummary.firstMessage?.id} threadId={threadId} />
 
       {showingReplies ? (
