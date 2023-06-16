588a5b86723cac69f49afc78b80dc54371cfa7cc
Josh Watzman <jwatzman@cord.com>
2023-06-16 15:16:16 +0100

    Notifications: add `useData` wrapper hook for `notification.observeData`
    A simple little wrapper. Documentation still forthcoming.
    
    
    Reviewers: flooey
    
    Reviewed By: flooey
    
    Pull Request: https://github.com/getcord/monorepo/pull/5124
    
    monorepo-commit: 89cdfbc19abd8740f04d86c22aa24c567c588048

--- packages/react/hooks/notification.ts
+++ packages/react/hooks/notification.ts
@@ -1,4 +1,5 @@
-import type { NotificationSummary } from '@cord-sdk/types';
+import type { NotificationData, NotificationSummary } from '@cord-sdk/types';
+import { useEffect, useState } from 'react';
 import { useCordContext } from '../contexts/CordContext';
 import { useNotificationSummaryInternal } from './useNotificationSummaryInternal';
 
@@ -7,4 +8,32 @@ export function useSummary(): NotificationSummary | null {
   const notificationSDK = sdk?.notification;
 
   return useNotificationSummaryInternal(notificationSDK, false);
+}
+
+const emptyNotificationData: NotificationData = {
+  notifications: [],
+  loading: true,
+  hasMore: false,
+  fetchMore: async (_: number) => {},
+};
+
+export function useData(): NotificationData {
+  const [data, setData] = useState<NotificationData | null>(null);
+
+  const { sdk } = useCordContext('useData');
+  const notificationSDK = sdk?.notification;
+
+  useEffect(() => {
+    if (!notificationSDK) {
+      return;
+    }
+
+    const key = notificationSDK.observeData(setData);
+
+    return () => {
+      notificationSDK.unobserveData(key);
+    };
+  }, [notificationSDK]);
+
+  return data ?? emptyNotificationData;
 }
