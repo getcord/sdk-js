90565d8cf59ac65285df56f030f81dd1c0a89a0c
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-06-16 16:21:39 +0100

    UI3: Convert NotificationList top level elements
    Another PR towards converting NotificationList to the new CSS world.
    
    
    Reviewers: flooey, jwatzman
    
    Reviewed By: jwatzman
    
    Pull Request: https://github.com/getcord/monorepo/pull/5137
    
    monorepo-commit: 41ed5e09365255f0c16143696ae8baf2269dcc16

--- packages/react/components/NotificationList.tsx
+++ packages/react/components/NotificationList.tsx
@@ -4,18 +4,21 @@ import {
   propsToAttributeConverter,
 } from '@cord-sdk/components';
 import type { NotificationListFilter } from '@cord-sdk/types';
-import type { ReactPropsWithStandardHTMLAttributes } from '../types';
+import type {
+  PropsWithFlags,
+  ReactPropsWithStandardHTMLAttributes,
+} from '../types';
 
 const propsToAttributes = propsToAttributeConverter(
   componentAttributes.NotificationList,
 );
 
-export type NotificationListReactComponentProps = {
+export type NotificationListReactComponentProps = PropsWithFlags<{
   maxCount?: number;
   fetchAdditionalCount?: number;
   showPlaceholder?: boolean;
   filter?: NotificationListFilter;
-};
+}>;
 
 export function NotificationList(
   props: ReactPropsWithStandardHTMLAttributes<NotificationListReactComponentProps>,
@@ -25,6 +28,7 @@ export function NotificationList(
       id={props.id}
       class={props.className}
       style={props.style}
+      use-shadow-root={props.useShadowRoot ?? false}
       {...propsToAttributes(props)}
     />
   );
