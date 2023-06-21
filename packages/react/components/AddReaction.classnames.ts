89ef4f57e7e01e095f4c9be609d8f2383aeedf73
Terrence Chung <56486667+Netceer@users.noreply.github.com>
2023-06-21 10:55:46 +0100

    Remove shadow root from Add Reaction and use newer components
    - Replace tooltip and button components with new ui3 versions with
    stable class names.
    - Remove the shadow root
    - Create class names file
    
    
    Reviewers: flooey, lazybean
    
    Reviewed By: lazybean
    
    Pull Request: https://github.com/getcord/monorepo/pull/5174
    
    monorepo-commit: f60ff30274979034e5521a062abc749c49f290b9

---
+++ packages/react/components/AddReaction.classnames.ts
@@ -0,0 +1,7 @@
+import { cordifyClassname } from '../common/util';
+
+export const addReaction = cordifyClassname('add-reaction');
+
+export const addReactionClassnamesDocs = {
+  [addReaction]: 'Applied to the add reaction button element.',
+};
