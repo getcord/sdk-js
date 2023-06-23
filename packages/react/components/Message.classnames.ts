e4b285933acd079002e653d2f383245e055b46c6
ludo <ludo@cord.com>
2023-06-23 13:54:46 +0200

    Move Message classenames to Message.classnames (#5225)
    Summary:
    This is useful for using those classes in other css.
    Plus could be used for doc
    
    
    Test Plan:
    Doc -> cord-message style properly styled
    
    Reviewers:
    
    monorepo-commit: 9ff2d42396ac3924db18817a5dddc94e6d33af63

---
+++ packages/react/components/Message.classnames.ts
@@ -0,0 +1,11 @@
+import { cordifyClassname } from "../common/util";
+
+export const message = cordifyClassname('message');
+export const authorNameContainer = cordifyClassname('author-name-container');
+export const authorName = cordifyClassname('author-name');
+export const sentViaIcon = cordifyClassname('sent-via-icon');
+export const optionsMenuTrigger = cordifyClassname('options-menu-trigger');
+export const reactions = cordifyClassname('reactions');
+export const undoDeletedText = cordifyClassname('undo-deleted-text');
+export const deletedMessageText = cordifyClassname('deleted-message-text');
+export const deletedIcon = cordifyClassname('deleted-icon');
