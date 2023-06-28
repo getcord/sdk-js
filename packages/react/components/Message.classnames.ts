631c3176457dbc967e6e7e1180a991cad63e7e77
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-06-28 16:32:58 +0100

    Docs: Document message classnames
    - Adding documentation for classnames
    - Removing an unused class ("reactions"). This has been replaced by its own component.
    - Renaming one class name (`undoDeletedText` -> `undoDeleteButton`)
    
    Test Plan:
    Load docs locally:
    <img width="1234" alt="image" src="https://github.com/getcord/monorepo/assets/33761650/251dd057-ffca-4969-b020-d4ff3410f56d">
    
    
    Reviewers: flooey, jwatzman
    
    Reviewed By: jwatzman
    
    Pull Request: https://github.com/getcord/monorepo/pull/5282
    
    monorepo-commit: c4c0c4206d3c220da978f0ed00a4aae68277bb39

--- packages/react/components/Message.classnames.ts
+++ packages/react/components/Message.classnames.ts
@@ -2,9 +2,31 @@ import { cordifyClassname } from '../common/util';
 
 export const message = cordifyClassname('message');
 export const authorName = cordifyClassname('author-name');
+export const messageTimestamp = cordifyClassname('message-timestamp');
+export const messageContent = cordifyClassname('message-content');
 export const sentViaIcon = cordifyClassname('sent-via-icon');
 export const optionsMenuTrigger = cordifyClassname('options-menu-trigger');
-export const reactions = cordifyClassname('reactions');
-export const undoDeletedText = cordifyClassname('undo-deleted-text');
+export const undoDeleteButton = cordifyClassname('undo-delete-button');
 export const deletedMessageText = cordifyClassname('deleted-message-text');
 export const deletedIcon = cordifyClassname('deleted-icon');
+
+export const messageClassnamesDocs = {
+  [message]:
+    'Applied to the container div. You can use `grid-template-areas` to modify the layout of the message.',
+  [authorName]:
+    'Applied to the div containing the name of the author of the message.',
+  [messageTimestamp]:
+    'Applied to the div containing the timestamp of the message.',
+  [messageContent]:
+    'Applied to the div containing the text of the message and any attachments.',
+  [optionsMenuTrigger]:
+    'Applied to the div which appears on hover on the top right of the message.',
+  [sentViaIcon]:
+    'Applied to the div which appears when the message was sent via Slack or via Email.',
+  [deletedIcon]:
+    'Applied to the div that contains the icon which appears when users delete a message.',
+  [deletedMessageText]:
+    'Applied to the div that contains the label that appears when users delete a message.',
+  [undoDeleteButton]:
+    'Applied to the "undo" button that appears when users delete a message.',
+};
