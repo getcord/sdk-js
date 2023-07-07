b38bfb3bdc5e304ebbfb4300ac0aff5050d8c5ca
khadija <127104309+km-nur@users.noreply.github.com>
2023-07-07 12:25:47 +0100

    REST API:  Add timestamp filters to GET Threads endpoint
    We currently have 2 filtering options - metadata and location.
    This diff adds two more options on top these - `first_message_timestamp`
    and `most_recent_message_timestamp`. We use these fields to match what we have
    for sorting on the Threads client-side API for consistency.
    
    Main difference these parameters have with the existing ones is the need
    for more validation to make sure users pass in correct date types to be
    used for checking against the db.
    
    
    Reviewers: dmmiller, flooey
    
    Reviewed By: flooey
    
    Pull Request: https://github.com/getcord/monorepo/pull/5338
    
    monorepo-commit: f0a9097cf13a3bba4d572888168cf6205c6b7463

--- packages/api-types/src/coreTypes.ts
+++ packages/api-types/src/coreTypes.ts
@@ -5,6 +5,16 @@ import type { EntityMetadata, Location } from '@cord-sdk/types';
  * @maxLength 128
  * */
 export type ID = string | number;
+export type TimestampRange = {
+  /**
+   * Timestamp from where to start the interval. If not present, the interval will have no start date and any data will include everything up to the provided `to` timestamp.
+   */
+  from?: Date;
+  /**
+   * Timestamp where to end the interval. If not present, the interval will have no end date and any data will include everything from the provided `from` timestamp.
+   */
+  to?: Date;
+};
 
 export type FilterParameters = {
   /**
@@ -15,4 +25,12 @@ export type FilterParameters = {
    * Arbitrary key-value pairs of data associated with the object.
    */
   metadata?: EntityMetadata;
+  /**
+   * Timestamp when the first message in a thread was created.
+   */
+  firstMessageTimestamp?: TimestampRange;
+  /**
+   * Timestamp when a message in a thread was last created or updated.
+   */
+  mostRecentMessageTimestamp?: TimestampRange;
 };
