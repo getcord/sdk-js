f74ef10b55eb956c9c071c55f02f9f1b3dccdfd8
ludo <ludo@cord.com>
2023-07-20 13:56:46 +0200

    SimpleNotificationWrapper (#5583)
    ui3/SimpleNotificationWrapper
    
    Summary:
    
    1. Create Notification.classnames because I'll need it.
    2. copypasta StructuredNotificationHeader + some cssification
    2. copypasta SimpleNotificationWrapper + some cssification
    
    It is using grid now, for easier customization.
    
    I use pseudo element for background instead to get rid of extra Box. 
    I wanted to use background color, but I want to preserve the existing user or css variables and would need not incredibly supporeted mix-color.
    
    
    
    
    I will convert the Menu in next PR, because this gets too big (🙇).
    
    
    
    
    
    
    Test Plan:
    
    Render, icon, avatar and bell by injecting state.
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/IoyKnJXsDnJREClofLDY/8f3dcfb3-6103-45d1-90c0-c0f0123bb0b3/image.png)
    
    ![image.png](https://graphite-user-uploaded-assets-prod.s3.amazonaws.com/IoyKnJXsDnJREClofLDY/7c3556e9-e342-48eb-911c-36e8e969ede1/image.png)
    
    
    Reviewers:
    
    monorepo-commit: 54336d690632ee520388d3bbf672997da32d3403

---
+++ packages/react/components/Notification.classnames.ts
@@ -0,0 +1,19 @@
+import { cordifyClassname } from '../common/util';
+
+export const notificationContainer = cordifyClassname('notification-container');
+export const notificationIconContainer = cordifyClassname(
+  'notification-icon-container',
+);
+export const notificationIcon = cordifyClassname('notification-icon');
+export const notificationHeaderContainer = cordifyClassname(
+  'notification-header',
+);
+export const notificationHeaderText = cordifyClassname(
+  'notification-header-text',
+);
+export const notificationHeaderUserMention = cordifyClassname(
+  'notification-header-user-mention',
+);
+export const notificationHeaderBold = cordifyClassname(
+  'notification-header-text-bold',
+);
