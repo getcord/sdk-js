22d9574c64ba320a9e340b130515e0bea5525b5c
Josh Watzman <jwatzman@cord.com>
2023-09-11 10:54:54 +0100

    Copy-paste fast-deep-equal directly into repo
    This path import doesn't work if you're using esm directly (or maybe
    doing so just with webpack?). In any event it doesn't work with the
    default configuration of a very popular tool.
    
    I'm pretty sick of figuring out all this esm vs cjs build system crap,
    default imports, etc etc. This module is small, let's copy-paste it in
    and be done with it.
    
    
    Test Plan:
    - Testbed still seems to work, especially `ThreadedComments` which I'm
      pretty sure hits this codepath.
    - Our rollup build works fine.
    - A default webpack project, which doesn't build with the
      currently-released version of `@cord-sdk/react`, works fine if you
      manually force a version with this change.
    
    Reviewers: dmmiller, flooey
    
    Reviewed By: flooey, dmmiller
    
    Pull Request: https://github.com/getcord/monorepo/pull/6351
    
    monorepo-commit: 1c2b6ade7bec9817c5628c4c26132746f1110513

--- packages/react/hooks/useMemoObject.ts
+++ packages/react/hooks/useMemoObject.ts
@@ -1,14 +1,5 @@
 import { useRef } from 'react';
-
-// For silly reasons, the main Cord repo has esModuleInterop off, which forces
-// the import x = require() syntax here. But rollup doesn't like that in an esm
-// build and requires the syntax below. The actual esbuild we do in the internal
-// repo is fine with either, so let's use the one that actually works for the
-// OSS rollup build and just tell TS to be quiet for our internal purposes in
-// this one case. (Instead of disentangling this properly which is a huge pain
-// and we've already wasted enough time on this build system crap.)
-// @ts-ignore
-import libIsEqual from 'fast-deep-equal/es6';
+import libIsEqual from '../common/lib/fast-deep-equal';
 
 /**
  * Returns a memoized version of the given complex JS object (such as an array
