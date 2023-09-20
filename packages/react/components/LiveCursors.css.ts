283be5fd20f2bbded1d52512264a607daca1e1bc
Josh Watzman <jwatzman@cord.com>
2023-09-20 11:51:15 +0100

    LiveCursors: styling improvements
    - Use Avatar to put a profile picture to the left of the user name.
    - Update background and border colors.
    
    
    Reviewers: AlbertQM, anothertompetty, flooey, lazybean
    
    Reviewed By: AlbertQM
    
    Pull Request: https://github.com/getcord/monorepo/pull/6471
    
    monorepo-commit: ab9e9b2b76c3f0c90c54951cfe1fe04caca5d873

--- packages/react/components/LiveCursors.css.ts
+++ packages/react/components/LiveCursors.css.ts
@@ -1,14 +1,18 @@
 import { globalStyle } from '@vanilla-extract/css';
 import { cordifyClassname } from '../common/util';
+import { cssVar } from '../common/ui/cssVariables';
+import * as avatarClasses from './Avatar.classnames';
 import { ZINDEX } from '@cord-sdk/react/common/ui/zIndex';
 
 export const POSITION_UPDATE_INTERVAL_MS = 100;
 
 export const cursor = cordifyClassname('live-cursors-cursor');
 export const icon = cordifyClassname('live-cursors-icon');
+export const label = cordifyClassname('live-cursors-label');
 export const name = cordifyClassname('live-cursors-name');
 
 export const colorVar = '--cord-live-cursors-cursor-color';
+export const borderVar = '--cord-live-cursors-cursor-border-color';
 
 globalStyle(`.${cursor}`, {
   // A small negative margin makes the pointer of the icon appear where the
@@ -25,16 +29,31 @@ globalStyle(`.${icon}`, {
   color: `var(${colorVar})`,
 });
 
-globalStyle(`.${name}`, {
+// Unfortunately 0-2-0 specificity -- Avatar isn't OSS so this style should live
+// here, and so there's no way to get a 0-1-0 here to consistently override it's
+// 0-1-0.
+globalStyle(`.${label} .${avatarClasses.avatarContainer}`, {
+  height: cssVar('space-xl'),
+  width: cssVar('space-xl'),
+  borderRadius: cssVar('space-xl'),
+
+  // Negative margin to pull the avatar a little more towards the left since
+  // it's rounded, but to let the label still look good/centered if there is no
+  // avatar.
+  marginLeft: '-6px',
+});
+
+globalStyle(`.${label}`, {
+  border: `1px solid var(${borderVar})`,
   borderRadius: '100px',
   color: 'white',
-  left: '10px',
-  paddingBottom: 2,
-  paddingLeft: 12,
-  paddingRight: 12,
-  paddingTop: 2,
+  padding: '4px 12px',
   position: 'absolute',
   top: '11px',
+  left: '10px',
   whiteSpace: 'nowrap',
   backgroundColor: `var(${colorVar})`,
+  display: 'flex',
+  alignItems: 'center',
+  gap: '8px',
 });
--- packages/react/components/LiveCursorsDefaultCursor.tsx
+++ packages/react/components/LiveCursorsDefaultCursor.tsx
@@ -5,6 +5,7 @@ import type {
 } from '@cord-sdk/types';
 import * as classes from './LiveCursors.css';
 import { Icon } from './helpers/Icon';
+import { Avatar } from './Avatar';
 
 type CursorPosition = NonNullable<LiveCursorsCursorPosition>;
 
@@ -21,7 +22,12 @@ export type LiveCursorsCursorProps = {
 };
 
 // The set of colors we rotate between as we need colors for people.
-const CURSOR_COLORS = ['#9A6AFF', '#EB5757', '#71BC8F', '#F88D76'];
+const CURSOR_COLORS = [
+  { background: '#0079FF', border: '#074B9C' },
+  { background: '#9A6AFF', border: '#6949AC' },
+  { background: '#9B3617', border: '#4E1908' },
+  { background: '#45CF7C', border: '#159347' },
+];
 
 // Index of the next CURSOR_COLORS to use when we see a new user.
 let colorIndex = 0;
@@ -29,7 +35,7 @@ let colorIndex = 0;
 // Saved colors of users (map from user ID to their color). We never delete from
 // this map so that a user's color is always consistent across a page load even
 // if their cursor goes away and comes back later.
-const colors: Record<string, string> = {};
+const colors: Record<string, (typeof CURSOR_COLORS)[number]> = {};
 
 function getColor(userID: string) {
   return (colors[userID] ??=
@@ -48,11 +54,15 @@ export function LiveCursorsDefaultCursor({
       style={{
         left: pos.x + 'px',
         top: pos.y + 'px',
-        [classes.colorVar as any]: color,
+        [classes.colorVar as any]: color.background,
+        [classes.borderVar as any]: color.border,
       }}
     >
       <Icon className={classes.icon} name="Cursor" size="large" />
-      <span className={classes.name}>{user.name}</span>
+      <div className={classes.label}>
+        <Avatar userId={user.id} />
+        <span className={classes.name}>{user.name}</span>
+      </div>
     </div>
   );
 }
