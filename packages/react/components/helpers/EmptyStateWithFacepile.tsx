d16fe0c1dde7ae9d4bb468f97f30792ec9f6acd0
Adam Vartanian <flooey@cord.com>
2023-09-22 14:47:56 +0100

    Restore the placeholder translations to EmptyStateWithFacepile (#6602)
    Put the translations back, now that we can translate opensource stuff.
    
    Test Plan:
    
    <img width="361" alt="Screenshot 2023-09-22 at 2 36 46 PM" src="https://github.com/getcord/monorepo/assets/215991/b0f145ab-24a7-43e1-94ce-e8889ab1db56">
    
    monorepo-commit: 39979f3be9c1ced8b08f69a168fc70b8448566a7

--- packages/react/components/helpers/EmptyStateWithFacepile.tsx
+++ packages/react/components/helpers/EmptyStateWithFacepile.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import cx from 'classnames';
 
+import { useTranslation } from 'react-i18next';
 import { Facepile } from '../Facepile';
 import * as classes from './EmptyStateWithFacepile.css';
 
@@ -10,16 +11,15 @@ type Props = {
 };
 
 export function EmptyStateWithFacepile({ users, className }: Props) {
+  const { t } = useTranslation('thread');
   return (
     <div className={cx(classes.emptyStatePlaceholderContainer, className)}>
       {users.length > 0 && <Facepile users={users} />}
       <p className={classes.emptyStatePlaceholderTitle}>
-        {'Chat with your team, right here'}
+        {t('placeholder_title')}
       </p>
       <p className={classes.emptyStatePlaceholderBody}>
-        {
-          "Ask a question, give feedback, or just say 'Hi'. Comments can be seen by anyone who can access this page."
-        }
+        {t('placeholder_body')}
       </p>
     </div>
   );
