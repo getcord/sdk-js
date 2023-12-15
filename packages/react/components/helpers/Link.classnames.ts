7046cdf02c8cbff3b7f5017ab7664aaaaa6429ab
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-12-15 14:30:26 +0000

    Opensource Link
    This is a very simple wrapper around `a`, but we'll need a react component to make it replaceable.
    
    Test Plan:
    Share a thread to Slack, re-open the Share to Slack menu, click on the "Shared to #.." link. It works.
    
    
    Reviewers: Netceer, lazybean
    
    Reviewed By: lazybean
    
    Pull Request: https://github.com/getcord/monorepo/pull/7624
    
    monorepo-commit: 518f1baf1b6f3cd78c0c3eace1b35fc500d5b467

---
+++ packages/react/components/helpers/Link.classnames.ts
@@ -0,0 +1,5 @@
+import { cordifyClassname } from '../../common/util';
+
+export const anchor = cordifyClassname('anchor');
+export const noUnderline = cordifyClassname('no-underline');
+export const ellipsis = cordifyClassname('ellipsis');
---
+++ packages/react/experimental/components/helpers/Link.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import type { AnchorHTMLAttributes } from 'react';
+import { forwardRef } from 'react';
+import cx from 'classnames';
+import * as classes from '../../../components/helpers/Link.classnames';
+
+export type LinkProps = React.PropsWithChildren<
+  AnchorHTMLAttributes<HTMLAnchorElement>
+>;
+export const Link = forwardRef(function Link(
+  { children, className, ...restProps }: LinkProps,
+  ref: React.Ref<HTMLAnchorElement>,
+) {
+  return (
+    <a ref={ref} className={cx(className, classes.anchor)} {...restProps}>
+      {children}
+    </a>
+  );
+});
