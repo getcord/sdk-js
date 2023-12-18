2e180f52109d4adb0f933bb4301889c1515cda6e
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-12-18 15:29:32 +0000

    Opensource BulletElement and MessageBulletElement
    Needed to de-onion the message component.
    Copied over these components to opensource (not experimental).
    
    
    Test Plan:
    Create an ordered and unordered list, send the message. Both still work.
    
    Reviewers: lazybean, Netceer
    
    Reviewed By: Netceer
    
    Pull Request: https://github.com/getcord/monorepo/pull/7619
    
    monorepo-commit: c51cdc8d9c3e38a1d8fff6d948602e417ed185e3

---
+++ packages/react/components/composer/BulletElement.tsx
@@ -0,0 +1,49 @@
+import * as React from 'react';
+import cx from 'classnames';
+import * as classes from '@cord-sdk/react/components/composer/BulletElement.classnames';
+
+type Props = {
+  children: React.ReactNode;
+} & (
+  | {
+      numberBullet: true;
+      bulletNumber: number;
+    }
+  | {
+      numberBullet?: false;
+      bulletNumber?: undefined;
+    }
+);
+
+export const BulletElement = ({
+  children,
+  numberBullet,
+  bulletNumber,
+}: Props) => {
+  const listItem = (
+    <li value={bulletNumber} className={classes.listItem}>
+      {children}
+    </li>
+  );
+
+  return (
+    <>
+      {numberBullet ? (
+        <ol
+          className={cx(classes.container, classes.orderedList)}
+          style={{
+            // Fix for "all: revert" CSS causing the list numbers to all be "1" in Firefox
+            // despite having the right "value" prop on the <li>
+            counterReset: `list-item ${bulletNumber - 1}`,
+          }}
+        >
+          {listItem}
+        </ol>
+      ) : (
+        <ul className={cx(classes.container, classes.unorderedList)}>
+          {listItem}
+        </ul>
+      )}
+    </>
+  );
+};
