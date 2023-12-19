b0180b29cc1d5467b492ff23afa398beca1a9150
Albert <33761650+AlbertQM@users.noreply.github.com>
2023-12-19 09:23:27 +0000

    Experimental MessageImageAttachment
    Opensourcing another type of attachment.
    
    Test Plan:
    `npm run tsc-once`
    
    
    Reviewers: lazybean, Netceer
    
    Reviewed By: Netceer
    
    Pull Request: https://github.com/getcord/monorepo/pull/7652
    
    monorepo-commit: 7650793d1507ffdd7794076a2df07d35ccc0715f

---
+++ packages/react/experimental/components/message/MessageImageAttachment.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import type { UploadedFile } from '@cord-sdk/types';
+import { ImageAttachment } from '../composer/ImageAttachment';
+
+type Props = {
+  file: UploadedFile;
+  onClick: () => void;
+};
+export function MessageImageAttachment({ onClick, file }: Props) {
+  const [fileDownloadFailed, setFileDownloadFailed] = useState(false);
+  const { url, name } = file;
+
+  const uploadState = file.uploadStatus;
+
+  const showErrorState =
+    uploadState === 'failed' ||
+    uploadState === 'cancelled' ||
+    fileDownloadFailed ||
+    // Author has optimistically-inserted dataURI right after posting a message
+    (uploadState === 'uploading' && !url.startsWith('data:'));
+
+  return (
+    <ImageAttachment
+      uploading={uploadState === 'uploading'}
+      url={url}
+      onClick={onClick}
+      onImageError={() => setFileDownloadFailed(true)}
+      showErrorState={showErrorState}
+      tooltipLabel={name}
+    />
+  );
+}
