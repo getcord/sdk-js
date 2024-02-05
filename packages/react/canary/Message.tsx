b0fab73fbfa9459e45a484b0e77eec4d8f28bdfc
ludo <ludo@cord.com>
2024-02-05 11:17:43 +0100

    add reaction to message (#8099)
    Summary:
    reuse classname from legacy and add reactions
    Test Plan:
    Use it in doc. try to add reaction from both buttons.
    Reviewers:
    
    monorepo-commit: c456ea430a09db40350e29b90d7721e056f2647e

--- packages/react/canary/Message.tsx
+++ packages/react/canary/Message.tsx
@@ -1,16 +1,22 @@
 import * as React from 'react';
-import { forwardRef } from 'react';
+import { forwardRef, useCallback, useContext } from 'react';
+import cx from 'classnames';
 import type { MessageContent as MessageContentType } from '@cord-sdk/types';
 import type { ClientMessageData } from '@cord-sdk/types/message.ts';
 import withCord from '../experimental/components/hoc/withCord.tsx';
+import * as classes from '../components/Message.classnames.ts';
+
 import {
   Avatar,
   Button,
   MessageContent,
   OptionsMenu,
   Timestamp,
+  AddReactionButton,
+  Reactions,
 } from '../experimental.ts';
 import { Icon } from '../components/helpers/Icon.tsx';
+import { CordContext } from '../contexts/CordContext.tsx';
 
 export type MessageProps = {
   message: ClientMessageData;
@@ -20,10 +26,34 @@ export const Message = withCord<React.PropsWithChildren<MessageProps>>(
   forwardRef(function Message({
     message,
     threadID,
+    className,
     ...restProps
   }: MessageProps) {
+    const { sdk: cordSDK } = useContext(CordContext);
+    const threadSDK = cordSDK?.thread;
+    const onAddReaction = useCallback(
+      (unicodeReaction: string) => {
+        if (threadSDK && threadID && message.id) {
+          void threadSDK.updateMessage(threadID, message.id, {
+            addReactions: [unicodeReaction],
+          });
+        }
+      },
+      [message.id, threadID, threadSDK],
+    );
+
+    const onDeleteReaction = useCallback(
+      (unicodeReaction: string) => {
+        if (threadSDK && threadID && message.id) {
+          void threadSDK.updateMessage(threadID, message.id, {
+            removeReactions: [unicodeReaction],
+          });
+        }
+      },
+      [message.id, threadID, threadSDK],
+    );
     return (
-      <div {...restProps}>
+      <div {...restProps} className={cx(className, classes.message)}>
         <Avatar canBeReplaced userId={message.authorID} />
         <Timestamp value={message.createdTimestamp} type="message" />
         <OptionsMenu
@@ -38,6 +68,11 @@ export const Message = withCord<React.PropsWithChildren<MessageProps>>(
           showThreadOptions
           showMessageOptions
         />
+        <AddReactionButton
+          messageId={message.id}
+          onAddReaction={onAddReaction}
+          onDeleteReaction={onDeleteReaction}
+        />
         <MessageContent
           // [ONI]-TODO: fix type in MessageContent and below
           // content is merged with HTMLAttributes<HTMLDivElement>['content']
@@ -46,6 +81,12 @@ export const Message = withCord<React.PropsWithChildren<MessageProps>>(
           // [ONI]-TODO: implement
           edited={false}
           canBeReplaced
+        />
+        <Reactions
+          messageId={message.id}
+          threadId={threadID}
+          showReactionList
+          showAddReactionButton
         />
       </div>
     );
