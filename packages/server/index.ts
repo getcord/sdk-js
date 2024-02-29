283e8ed2815fabc36d859d4b244a3345bfaa7568
Adam Vartanian <flooey@cord.com>
2024-02-29 13:45:24 +0000

    Allow token expiry to be configured (#8308)
    Add the ability to configure the token expiry in our JS server
    package.
    
    Test Plan:
    
    Generated a token with 24h expiry and decoded it, the token had the
    right `exp` value.
    
    monorepo-commit: c5e8a11d8b8f647092ba20378d6e4a00f48e76c9

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,10 +3,24 @@ import type { ClientAuthTokenData } from '@cord-sdk/api-types';
 
 export type { ClientAuthTokenData };
 
+export type CommonAuthTokenOptions = {
+  /**
+   * How long until the token expires.  If not set, defaults to 1 minute.
+   */
+  expires?: jwt.SignOptions['expiresIn'];
+};
+
+export type GetClientAuthTokenOptions = CommonAuthTokenOptions;
+
+export type GetServerAuthTokenOptions = CommonAuthTokenOptions;
+
+export type GetApplicationManagementAuthTokenOptions = CommonAuthTokenOptions;
+
 export function getClientAuthToken(
   project_id: string,
   project_secret: string,
   payload: Omit<ClientAuthTokenData, 'app_id' | 'project_id'>,
+  options: GetClientAuthTokenOptions = {},
 ): string {
   if (!payload || !payload.user_id) {
     // You can't get here in TS -- it's a TS type error -- but not everyone uses
@@ -21,27 +35,29 @@ export function getClientAuthToken(
 
   return jwt.sign({ ...payload, project_id }, project_secret, {
     algorithm: 'HS512',
-    expiresIn: '1 min',
+    expiresIn: options.expires ?? '1 min',
   });
 }
 
 export function getServerAuthToken(
   project_id: string,
   project_secret: string,
+  options: GetServerAuthTokenOptions = {},
 ): string {
   return jwt.sign({ app_id: project_id }, project_secret, {
     algorithm: 'HS512',
-    expiresIn: '1 min',
+    expiresIn: options.expires ?? '1 min',
   });
 }
 
 export function getApplicationManagementAuthToken(
   customer_id: string,
   customer_secret: string,
+  options: GetApplicationManagementAuthTokenOptions = {},
 ): string {
   return jwt.sign({ customer_id }, customer_secret, {
     algorithm: 'HS512',
-    expiresIn: '1 min',
+    expiresIn: options.expires ?? '1 min',
   });
 }
 
