ea3eaf8ebc7f3b7b35abf999b71f7707c06a80d8
ludo <ludo@cord.com>
2024-03-20 09:11:50 +0100

    the onSubmit needs to get all message data (#8505)
    Summary:
    onSubmit is passed the whole message.
    This makes easy to extand composer to add reactions, metadata, our extraClassnames without havint to rewrite the onSubmit
    
    
    Test Plan:
    Add a button that pass aditional reactions and metadata to onSubmit (pr #8419)
    
    Reviewers:
    
    monorepo-commit: 55b74f84bbdf9ea36e9d761845cc0fdb26c078d3

--- packages/react/canary/composer/hooks/useSubmit.ts
+++ packages/react/canary/composer/hooks/useSubmit.ts
@@ -6,11 +6,12 @@ import type {
   ClientMessageData,
   MessageAttachment,
   MessageFileAttachment,
+  Reaction,
 } from '@cord-sdk/types';
-
 import { CordContext } from '../../../contexts/CordContext.js';
 
 const EMPTY_ATTACHMENTS: MessageAttachment[] = [];
+const EMPTY_REACTIONS: Reaction[] = [];
 
 type UseEditSubmit = {
   initialValue?: Partial<ClientMessageData>;
@@ -25,18 +26,27 @@ export function useEditSubmit(args: UseEditSubmit) {
   const { sdk: cord } = useContext(CordContext);
   return useCallback(
     ({ message }: { message: Partial<ClientMessageData> }) => {
+      const { reactions, attachments, ...restMessage } = message;
+
       const initialFileAttachments = initialAttachments.filter(
         isMessageFileAttachment,
       );
-      const filesAttachments = (message.attachments ?? []).filter(
+      const filesAttachments = (attachments ?? []).filter(
         isMessageFileAttachment,
       );
       const oldAttachmentIDs = new Set(
         initialFileAttachments?.map((a) => a.id),
       );
       const newAttachmentIDs = new Set(filesAttachments?.map((a) => a.id));
+      const initialReactions = new Set(
+        (initialValue?.reactions ?? []).map((r) => r.reaction) ??
+          EMPTY_REACTIONS,
+      );
+      const newReactions = new Set(
+        (reactions ?? []).map((r) => r.reaction) ?? EMPTY_REACTIONS,
+      );
       void cord?.thread.updateMessage(threadId, messageId, {
-        content: message.content,
+        ...restMessage,
         addAttachments: filesAttachments
           .filter((a) => !oldAttachmentIDs.has(a.id))
           .map((a) => ({ id: a.id, type: 'file' })),
@@ -44,10 +54,23 @@ export function useEditSubmit(args: UseEditSubmit) {
           initialFileAttachments
             ?.filter((a) => !newAttachmentIDs.has(a.id))
             .map((a) => ({ id: a.id, type: 'file' })) ?? [],
-        extraClassnames: message.extraClassnames,
+        addReactions:
+          (reactions ?? [])
+            .filter((r) => !initialReactions.has(r.reaction))
+            .map((r) => r.reaction) ?? [],
+        removeReactions:
+          (initialValue?.reactions ?? [])
+            .filter((r) => !newReactions.has(r.reaction))
+            .map((r) => r.reaction) ?? [],
       });
     },
-    [cord?.thread, messageId, threadId, initialAttachments],
+    [
+      cord?.thread,
+      messageId,
+      threadId,
+      initialAttachments,
+      initialValue?.reactions,
+    ],
   );
 }
 
@@ -62,17 +85,19 @@ export function useCreateSubmit(args: UseCreateSubmit) {
   const { sdk: cord } = useContext(CordContext);
   return useCallback(
     ({ message }: { message: Partial<ClientMessageData> }) => {
-      const filesAttachments = (message.attachments ?? []).filter(
+      const { reactions, content, attachments, ...restMessage } = message;
+      const filesAttachments = (attachments ?? []).filter(
         isMessageFileAttachment,
       );
       const url = window.location.href;
       void cord?.thread.sendMessage(threadId ?? uuid(), {
-        content: message.content ?? [],
+        ...restMessage,
+        content: content ?? [],
         addAttachments: filesAttachments.map((a) => ({
           id: a.id,
           type: 'file',
         })),
-        extraClassnames: message.extraClassnames,
+        addReactions: (reactions ?? []).map((r) => r.reaction),
         createThread: createThread ?? {
           location: { location: url },
           url,
