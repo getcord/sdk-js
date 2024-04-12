c794280f3f5bfcdea81f184640bc7b6f80134cf6
Elena Pérez Rioja <102948032+elenaperezrioja@users.noreply.github.com>
2024-04-12 09:24:41 +0200

    Facepile accepts user data or user ids (#8690)
    ## Summary
    
    Similar to Avatar, Facepile only accepted user ids. If customer want to set custom urls, they can't. So making that available.
    
    ## Test plan
    
    Check the testbed. Nothing really changes.
    
    monorepo-commit: d413c874613bde03188f97bfa7891b8bc8dc51e2

--- packages/react/experimental/components/Facepile.tsx
+++ packages/react/experimental/components/Facepile.tsx
@@ -1,43 +1,68 @@
 import * as React from 'react';
 import { useMemo, forwardRef } from 'react';
 import cx from 'classnames';
+import type { ClientUserData } from '@cord-sdk/types';
 import { isDefined } from '../../common/util.js';
 import { useUserData } from '../../hooks/user.js';
 import classes from '../../components/Facepile.css.js';
+import type {
+  ByID,
+  StyleProps,
+  WithByIDComponent,
+} from '../../experimental.js';
 import withCord from './hoc/withCord.js';
 import { Avatar } from './avatar/Avatar.js';
 
 export type FacepileProps = {
+  users: ClientUserData[];
+} & CommonFacepileProps;
+
+export type FacepileByIDProps = {
   userIDs: string[];
-  enableTooltip?: boolean;
-  className?: string;
-};
+} & CommonFacepileProps;
 
-export const Facepile = withCord<React.PropsWithChildren<FacepileProps>>(
-  forwardRef(function Facepile(
-    { className, userIDs, enableTooltip = true }: FacepileProps,
-    ref?: React.ForwardedRef<HTMLDivElement>,
-  ) {
-    const usersDataById = useUserData(userIDs);
-    const usersData = useMemo(
-      () => Object.values(usersDataById).filter(isDefined),
-      [usersDataById],
-    );
+type CommonFacepileProps = { enableTooltip?: boolean } & StyleProps;
 
-    if (!usersData) {
-      return null;
-    }
-    return (
-      <div className={cx(classes.facepileContainer, className)} ref={ref}>
-        {userIDs.map((userID) => (
-          <Avatar.ByID
-            key={userID}
-            userID={userID}
-            enableTooltip={enableTooltip}
-          />
-        ))}
-      </div>
-    );
-  }),
-  'Facepile',
-);
+export const Facepile: WithByIDComponent<FacepileProps, FacepileByIDProps> =
+  Object.assign(
+    withCord<React.PropsWithChildren<FacepileProps>>(
+      forwardRef(function Facepile(
+        { className, users, enableTooltip = true }: FacepileProps,
+        ref?: React.ForwardedRef<HTMLDivElement>,
+      ) {
+        const usersData = useMemo(
+          () => Object.values(users).filter(isDefined),
+          [users],
+        );
+
+        if (!usersData) {
+          return null;
+        }
+        return (
+          <div className={cx(classes.facepileContainer, className)} ref={ref}>
+            {usersData.map((user) => (
+              <Avatar
+                key={user.id}
+                user={user}
+                enableTooltip={enableTooltip}
+                canBeReplaced
+              />
+            ))}
+          </div>
+        );
+      }),
+      'Facepile',
+    ),
+    { ByID: FacepileByID },
+  );
+
+function FacepileByID(props: ByID<FacepileByIDProps>) {
+  const { userIDs, ...restProps } = props;
+  const usersDataById = useUserData(userIDs);
+
+  const data = userIDs
+    .map((id) => usersDataById[id])
+    .filter(Boolean) as ClientUserData[];
+
+  return <Facepile users={data} {...restProps} canBeReplaced />;
+}
