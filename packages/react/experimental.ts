3bf8bcf8ed968490cfd8221f9e8f17797bb8a333
My Hoa Tien <59407030+mhtien@users.noreply.github.com>
2024-04-12 10:26:31 +0100

    Export menu button in experiemental
    Would like to use Ludo's lovely new component in
    a composer layout example that will be on the docs.
    
    Test Plan:
    Can now use it when I try to import it
    in the docs.
    
    
    Reviewers: lazybean, elenaperezrioja, AlbertQM, Netceer
    
    Reviewed By: AlbertQM
    
    Pull Request: https://github.com/getcord/monorepo/pull/8717
    
    monorepo-commit: 9058306b9f6a4395270eb70cdc8cc3e41e2cb63d

--- packages/react/experimental.ts
+++ packages/react/experimental.ts
@@ -39,7 +39,12 @@ export {
   Button,
   type GeneralButtonProps,
 } from './experimental/components/helpers/Button.js';
-export { Menu, type MenuProps } from './experimental/components/menu/Menu.js';
+export {
+  Menu,
+  type MenuProps,
+  MenuButton,
+  type MenuButtonProps,
+} from './experimental/components/menu/Menu.js';
 export {
   MenuItem,
   type MenuItemProps,
