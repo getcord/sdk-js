904f41f36ee49e9fb9688be2180c0a243ee22a41
Albert <33761650+AlbertQM@users.noreply.github.com>
2024-05-10 16:40:40 +0100

    Composer 4.0: Cleanup and document classes
    - Some classes are not in use in the new version (4.0) of the component. E.g. the slot (web-component related) the placeholder (we don't have a custom placeholder in 4.0) and the menu (we have a toolbar now).
    - 4.0 docs should import from `opensource`, as those are separate styles from external/ui3
    
    
    Test Plan: Load docs locally
    
    Reviewers: mhtien
    
    Reviewed By: mhtien
    
    Pull Request: https://github.com/getcord/monorepo/pull/9062
    
    monorepo-commit: fa1bc6caa99a272336c1053721e0215d2a4abdb4

--- packages/react/components/Composer.classnames.ts
+++ packages/react/components/Composer.classnames.ts
@@ -11,7 +11,6 @@ export const valid = cordifyClassname('valid');
 export const alwaysExpand = cordifyClassname('always-expand');
 export const neverExpand = cordifyClassname('never-expand');
 export const autoExpand = cordifyClassname('auto-expand');
-export const composerButtonsContainer = cordifyClassname('composer-menu');
 export const primaryButtonsGroup = cordifyClassname('composer-primary-buttons');
 export const secondaryButtonsGroup = cordifyClassname(
   'composer-secondary-buttons',
@@ -20,11 +19,28 @@ export const composerErrorMessage = cordifyClassname('composer-error-message');
 
 export const editorContainer = cordifyClassname('editor-container');
 export const editor = cordifyClassname('editor');
-export const editorSlot = cordifyClassname('editor-slot');
-export const placeholder = cordifyClassname('placeholder');
 
 export const userReferenceSuggestionsMenu = cordifyClassname('mention-menu');
 
 export const attachmentsContainer = cordifyClassname('attachments');
 export const hasAttachments = cordifyClassname('has-attachments');
 export const collapsedComposerSelector = `.${composerContainer}:not(.${hasAttachments}):is(.${neverExpand}, .${autoExpand}:not(:focus-within))`;
+
+export const composerClassnamesDocs = {
+  [composerContainer]:
+    'Applied to the container div. This class is always present.',
+  [empty]: 'Applied to the container div when the text editor is empty',
+  [valid]:
+    'Applied to the container div when the message is valid and can be sent. This means there either some non-whitespace text, or at least one attachment.',
+  [alwaysExpand]:
+    'Applied to the container div, reflects the composer `expanded` prop.',
+  [neverExpand]:
+    'Applied to the container div, reflects the composer `expanded` prop.',
+  [autoExpand]:
+    'Applied to the container div, reflects the composer `expanded` prop.',
+  [editorContainer]: 'Applied to the div containing the editor.',
+  [attachmentsContainer]:
+    'Applied to the div containing the attachments (files and images).',
+  [composerErrorMessage]:
+    'Applied to the div containing the error message that appears when a message fails to send',
+};
