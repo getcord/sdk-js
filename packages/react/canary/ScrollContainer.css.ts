8e469d5459a3300f3d3853df2bff67668f9eeecb
My Hoa Tien <59407030+mhtien@users.noreply.github.com>
2024-05-10 18:11:07 +0200

    Add CSS for ScrollContainer
    Noticed we had a gap between the threads in ThreadedComments
    and we don't have one in threads now.
    
    Also remove the :where from the thread CSS, and
    updated to add flex to get the gap working in the
    existing thread CSS.
    
    Test Plan:
    Can see gap locally!
    
    Reviewers: AlbertQM
    
    Reviewed By: AlbertQM
    
    Pull Request: https://github.com/getcord/monorepo/pull/9064
    
    monorepo-commit: ed1de76e67c5d7604441462805afcf2a999639a8

--- packages/react/canary/ScrollContainer.css.ts
+++ packages/react/canary/ScrollContainer.css.ts
@@ -2,17 +2,24 @@ import { cssVar } from '../common/ui/cssVariables.js';
 import { globalStyle } from '../common/ui/style.js';
 import { cordifyClassname } from '../common/util.js';
 import { thread } from '../components/Thread.classnames.js';
+import { threads } from './threads/Threads.classnames.js';
 
 export const scrollContainer = cordifyClassname('scroll-container');
 globalStyle(`.${scrollContainer}`, {
-  position: 'relative',
+  display: 'flex',
+  flexDirection: 'column',
+  flexShrink: 1,
   overflow: 'auto',
   overscrollBehavior: 'contain',
-  flexShrink: 1,
+  position: 'relative',
 });
 
-globalStyle(`:where(.${thread}) .${scrollContainer}`, {
+globalStyle(`.${thread} .${scrollContainer}`, {
   gap: cssVar('space-2xs'),
   padding: cssVar('space-3xs'),
   paddingBottom: cssVar('space-2xs'),
+});
+
+globalStyle(`.${threads} .${scrollContainer}`, {
+  gap: cssVar('space-2xs'),
 });
